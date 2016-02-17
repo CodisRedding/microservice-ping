a87ad19c4e5011fee6531926fa6d7874b936d4ad
Rocky Assad <rocky@fourq.io>
2016-02-17 09:55:46 -0600

    add testingj

--- api/ping.js
+++ api/ping.js
@@ -2,6 +2,7 @@
 
 
 module.exports = {
+  /* eslint no-unused-vars: 0 */
   ping: (callback, header, body, data) => {
     callback('ping', `${header} pong`);
   }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const net = require('net');
-const os = require('os');
 const adaptor = require('service-adapter');
 const config = require('config');
 const api = require('./api');
@@ -9,7 +8,6 @@ const api = require('./api');
 
 net
   .createServer((serverSocket) => {
-    console.log('s onConnect');
     serverSocket
       .pipe(new adaptor(api.ping))
       .pipe(serverSocket);
@@ -17,9 +15,7 @@ net
   .listen(config.get('port'), config.get('host'), (err) => {
     if (err != null) {
       // TODO: logging service
-      console.log('[PING microservice] - error:', err);
     }
 
     // TODO: logging service
-    console.log('[PING microservice] - started on port', config.get('host') + ':' + config.get('port'));
   });
